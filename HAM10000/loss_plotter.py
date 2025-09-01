81688aa0f33860e426a2554226fce53753e32133
Konstantina Sofia Charalampeli <126577929+sofcony@users.noreply.github.com>
2025-09-01 10:40:42 +0300

    Add main scripts via upload

---
+++ HAM10000/loss_plotter.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import os
+import re
+from checkpoint_config import deconstruct_dir # type: ignore
+
+def loss_plotter(dir_path):
+    # Deconstruct the directory path to retrieve hyperparameters
+    seed, criter, optim, sched, learning_rate, dim_list, grid_size, grid_min, grid_max, inv_denominator = deconstruct_dir(dir_path)
+    
+    file_path = os.path.join(dir_path, 'loss_logs.txt')
+    with open(file_path, 'r') as file:
+        data = file.readlines()
+
+    # Extract the data using regular expressions
+    epochs = []
+    training_acc = []
+    validation_acc = []
+
+    for line in data:
+        match = re.match(r"Epoch (\d+): Training Loss = ([\d.]+), Validation Loss = ([\d.]+)", line)
+        if match:
+            epochs.append(int(match.group(1)))
+            training_acc.append(float(match.group(2)))
+            validation_acc.append(float(match.group(3)))
+    
+    # Find the minimum validation loss for this model
+    min_validation_loss = min(validation_acc) if validation_acc else float('inf')
+
+    # Find the epoch(s) where the validation loss is minimum
+    min_loss_epoch = validation_acc.index(min_validation_loss)
+    min_loss_value = validation_acc[min_loss_epoch]
+
+    # Plot the data for the first num epochs
+    num = -1
+    plt.figure(figsize=(8, 5))
+    plt.plot(epochs[:num], training_acc[:num], label='Training Loss', marker='o', color = 'deepskyblue')
+    plt.plot(epochs[:num], validation_acc[:num], label='Validation Loss', marker='o', color = 'red')
+
+    # Plot the minima with markers
+    plt.scatter(epochs[min_loss_epoch], min_loss_value, color='black', edgecolor='white', s=100, zorder=5, label='Min Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Loss Diagram')
+    plt.figtext(0.5, 0.001, 
+                f'Model Hyperparams:\nSeed = {seed}, Criterion = {criter}, Optimizer = {optim}, Scheduler = {sched},\n'
+                f'Learning Rate = {learning_rate}, Grid Size = {grid_size}, # of FasterKAN Layers = {len(dim_list)-1},\n'
+                f'Dimension List = {str(dim_list)}, Grid Min = {grid_min}, Grid Max = {grid_max}, Inv Denominator = {inv_denominator}', 
+                ha='center', va='top', fontsize=10, wrap=True)
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(os.path.join(dir_path, f'loss_plot.jpg'))
+    plt.show()
